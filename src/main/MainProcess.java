36ce954f4453b80fcf70c11ee11a0c96834abb8d
verriva <echeverri.va@gmail.com>
2020-01-18 15:19:32 -0600

    Seccond Commit

---
+++ src/main/MainProcess.java
@@ -0,0 +1,12 @@
+package main;
+
+public class MainProcess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola usuario bienvenido");
+        System.out.println("A continuación se muestra una cuenta regresiva");
+
+	}
+
+}
